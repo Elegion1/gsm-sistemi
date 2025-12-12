57e60f2f6add3f9de8152b5fa0e9a249182bf260
Giovanni Sugamiele <gionnymiele@gmail.com>
2025-12-12 13:29:41 +0100

    correzione bug

--- src/app/components/CookieConsentBanner.js
+++ src/app/components/CookieConsentBanner.js
@@ -66,7 +66,7 @@ export default function CookieConsentBanner({
       <div className="container d-flex flex-column flex-md-row justify-content-between align-items-center">
         <div className="mb-2 mb-md-0">
           {message}{" "}
-          <a href="/privacy" target="_blank" rel="noopener noreferrer">
+          <a href="/privacy-terms" target="_blank" rel="noopener noreferrer">
             Privacy Policy
           </a>
         </div>
